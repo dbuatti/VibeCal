709e7135cc08df139b7a596e4721d7c129f89cfe
[dyad] <daniele.buatti@gmail.com>
2026-04-17 17:43:45 +1000

    [dyad] wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,8 +15,8 @@ const Login = () => {
           access_type: 'offline',
           prompt: 'consent',
         },
-        // Requesting permission to read/write calendar events
-        scopes: 'https://www.googleapis.com/auth/calendar.events'
+        // Requesting permission to read calendars and manage events
+        scopes: 'https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events'
       }
     });
   };
@@ -53,7 +53,7 @@ const Login = () => {
                   fill="#FBBC05"
                 />
                 <path
-                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 12-4.53z"
+                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 12-4.53z"
                   fill="#EA4335"
                 />
               </svg>
